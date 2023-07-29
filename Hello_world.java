9871b6d4a53585281a6335ea68bce02852141753
CLAUDIO CONTI RIBEIRO <claudio_ribeiro2@estudante.sc.senai.br>
2023-07-28 21:57:14 -0300

    Primeiro Commit

---
+++ Hello_world.java
@@ -0,0 +1,16 @@
+// Primeiro código em java 
+/* 
+   Comentário
+   em
+   Bloco
+*/
+
+// APENAS AS CLASSES COMEÇAM COM MAIUSCULA
+// TODAS AS INSTRUÇÕES TEM QUE TERMINAR COM PONTO E VIRGULA ;
+
+public class Hello_world {
+	// Método principal 
+	public static void main(String[] args){
+		System.out.println("Batata Assada v3");
+	} // Fim do método main
+} // Fim fa clase Hello_world
---
+++ Hello_world2.java
@@ -0,0 +1,7 @@
+public class Hello_world2 {
+	// Método principal
+	public static void main(String[] args){
+		System.out.println("Olá, bem vindo ao inferno!");
+		System.out.print("Batata assada v3");
+	}
+}
